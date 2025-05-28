6fba098064f37b871f024fd8aa087c53cfea9a4b
john-papani <johnpapani1@gmail.com>
2025-05-28 19:30:49 +0300

    Add navbar

---
+++ src/app/Components/Navbar.js
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <header className="bg-zinc-900 shadow-md fixed top-0 left-0 w-full z-50">
+      <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
+        <h1 className="text-2xl font-bold text-white tracking-wide cursor-pointer hover:opacity-80 transition">
+          Github Explorer
+        </h1>
+      </div>
+    </header>
+  );
+}
